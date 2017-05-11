a60e75b35d5d85f83aa5e62fcafd81a167f9d528
jhjang04 <jjh920415@naver.com>
2017-05-11 22:21:24 +0900

    mybox start

---
+++ resource/mybox/copydat.sh
@@ -0,0 +1,4 @@
+cp ../../../../../../resource/mybox/*.dat ./
+
+
+
---
+++ resource/mybox/test.py
@@ -0,0 +1,6 @@
+import time
+print "TEST PYTHON"
+print "SLEEP.............."
+time.sleep(3)
+print "WAKE UP!!"
+print "TEST PYTHON END"
